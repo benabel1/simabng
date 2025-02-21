7580a07a532d9f1457859276eac6077b72bb1f74
benabel1 <xblazko@gmail.com>
2025-02-21 18:08:05 +0100

    Better logging, better printing players

--- main/src/main/java/org/example/game/cards/blue/valley/CardRattlesnake.java
+++ main/src/main/java/org/example/game/cards/blue/valley/CardRattlesnake.java
@@ -1,5 +1,6 @@
 package org.example.game.cards.blue.valley;
 
+import org.example.game.cards.GameCard;
 import org.example.game.cards.PokerValue;
 import org.example.game.cards.IsRevealAble;
 import org.example.game.cards.Suit;
@@ -20,5 +21,10 @@ public class CardRattlesnake extends BlueBorderCard implements IsRevealAble {
     @Override
     public String getPriorityType() {
         return getCardName();
+    }
+
+    @Override
+    public boolean matchSuitAndPoker(GameCard deckAble) {
+        return false;
     }
 }
